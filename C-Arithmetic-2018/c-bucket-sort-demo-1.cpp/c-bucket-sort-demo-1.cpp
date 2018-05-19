a731b1d77c40e383c26c86212b97ff3a5e8bab29
asm32 <asm32@qq.com>
2018-05-19 10:55:37 +0800

    c-bucket-sort-demo-1.cpp ... add

--- C-Arithmetic-2018/c-bucket-sort-demo-1.cpp/c-bucket-sort-demo-1.cpp
+++ C-Arithmetic-2018/c-bucket-sort-demo-1.cpp/c-bucket-sort-demo-1.cpp
@@ -1,36 +1,32 @@
 // c-bucket-sort-demo-1.cpp
 #include <iostream>
-#include <stdio.h>
-#include <malloc.h>
 // #include <time.h>
 
 using namespace std;
 
-void PA_DisplayData(int data[], int nCount){
-    for(int i = 0; i < nCount; i++){
-        if(i) printf(", ");
-        printf("%d", data[i]);
-    }
-    printf("\n");
-}
-
 
 const int MAX = 100;
 const int bn = 5;
-
 const int nFactor = MAX % bn ? MAX / bn + 1 : MAX / bn;
-
 int C[bn];
 
-void InsertionSort(int A[], int left, int right){
-    for(int i = left + 1; i <= right; i++){
-        int get = A[i];
+void PA_DisplayData(int data[], int nCount){
+    for(int i = 0; i < nCount; i++){
+        if(i) cout << ", ";
+        cout << data[i];
+    }
+    cout << endl;
+}
+
+void InsertionSort(int A[], int nLeft, int nRight){
+    for(int i = nLeft + 1; i <= nRight; i++){
+        int nGet = A[i];
         int j = i - 1;
-        while(j >= left && A[j] > get){
+        while(j >= nLeft && A[j] > nGet){
             A[j + 1] = A[j];
             j--;
         }
-        A[j + 1] = get;
+        A[j + 1] = nGet;
     }
 }
 
@@ -48,7 +44,7 @@ void CountingSort(int A[], int n){
     for(int i = 1; i < bn; i++){
         C[i] = C[i] + C[i - 1];
     }
-    int * B = (int *)malloc(n * sizeof(int));
+    int * B = new int[n];
     for(int i = n - 1; i >= 0; i--){
         int b = MapToBucket(A[i]);
         B[--C[b]] = A[i];
@@ -56,35 +52,35 @@ void CountingSort(int A[], int n){
     for(int i = 0; i < n; i++){
         A[i] = B[i];
     }
-    free(B);
+    delete B;
 }
 
 void BucketSort(int A[], int n){
     CountingSort(A, n);
     for(int i = 0; i < bn; i++){
-        int left = C[i];
-        int right = (i == bn - 1 ? n - 1 : C[i + 1] - 1); // C[i + 1] - 1为i号桶最后一个元素的位置
-        if(left < right){
-            InsertionSort(A, left, right);
+        int nLeft = C[i];
+        int nRight = (i == bn - 1 ? n - 1 : C[i + 1] - 1); // C[i + 1] - 1为i号桶最后一个元素的位置
+        if(nLeft < nRight){
+            InsertionSort(A, nLeft, nRight);
         }
     }
 }
 
 int main(){
-    // int _source[] = {41, 67, 34, 0, 69, 24, 78, 58, 62, 64, 5, 45, 81, 27, 61, 91, 95, 42, 27, 36};
-    int _source[] = {76, 11, 11, 43, 78, 35, 39, 27, 16, 55, 1, 41, 24, 19, 54, 7, 78, 69, 65, 82};
-    int nCount = sizeof(_source) / sizeof(int);
+    // int data[] = {41, 67, 34, 0, 69, 24, 78, 58, 62, 64, 5, 45, 81, 27, 61, 91, 95, 42, 27, 36};
+    int data[] = {76, 11, 11, 43, 78, 35, 39, 27, 16, 55, 1, 41, 24, 19, 54, 7, 78, 69, 65, 82};
+    int nCount = sizeof(data) / sizeof(int);
 
-    printf("Item count: %d\n", nCount);
+    cout << "Item count: " << nCount << endl;
 
     // srand(time(NULL));
     // for(int i = 0; i < 20; i++){
-    //     _source[i] = rand() % 1000;
+    //     data[i] = rand() % 1000;
     // }
 
-    PA_DisplayData(_source, nCount);
-    BucketSort(_source, nCount);
-    PA_DisplayData(_source, nCount);
+    PA_DisplayData(data, nCount);
+    BucketSort(data, nCount);
+    PA_DisplayData(data, nCount);
     
     return 0;
 }
