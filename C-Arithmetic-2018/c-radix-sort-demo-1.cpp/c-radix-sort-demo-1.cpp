d5af1b2bcb8036faae2245e1200d3addcbdfc511
asm32cn <asm32@qq.com>
2018-05-22 02:40:13 +0800

    c-radix-sort-demo-1.cpp ... add

--- C-Arithmetic-2018/c-radix-sort-demo-1.cpp/c-radix-sort-demo-1.cpp
+++ C-Arithmetic-2018/c-radix-sort-demo-1.cpp/c-radix-sort-demo-1.cpp
@@ -3,51 +3,54 @@
 
 using namespace std;
 
-void PA_DisplayData(int data[], int nCount){
-    for(int i = 0; i < nCount; i++){
-        if(i) cout << ", ";
-        cout << data[i];
-    }
-    cout << endl;
-}
+class LsdRedixSortDemo1{
+private:
+    const static int dn = 3;
+    const static int k = 10;
+    int C[k];
 
+public:
+    void PA_DisplayData(int data[], int nCount){
+        for(int i = 0; i < nCount; i++){
+            if(i) cout << ", ";
+            cout << data[i];
+        }
+        cout << endl;
+    }
 
+    void CountingSort(int A[], int n, int d){
+        for(int i = 0; i < k; i++){
+            C[i] = 0;
+        }
+        for(int i = 0; i < n; i++){
+            C[GetDigit(A[i], d)]++;
+        }
+        for(int i = 1; i < k; i++){
+            C[i] = C[i] + C[i - 1];
+        }
+        int *B = new int[n];
+        for(int i = n - 1; i >= 0; i--){
+            int dight = GetDigit(A[i], d);
+            B[--C[dight]] = A[i];
+        }
+        for(int i = 0; i < n; i++){
+            A[i] = B[i];
+        }
+        delete B;
+    }
 
-const int dn = 3;
-const int k = 10;
-int C[k];
+    void LsdRedixSort(int A[], int n){
+        for(int d = 1; d < dn; d++){
+            CountingSort(A, n, d);
+        }
+    }
 
-int GetDigit(int x, int d){
-    int radix[] = {1, 1, 10, 100};
-    return (x / radix[d]) % 10;
-}
-
-void CountingSort(int A[], int n, int d){
-    for(int i = 0; i < k; i++){
-        C[i] = 0;
+private:
+    int GetDigit(int x, int d){
+        int radix[] = {1, 1, 10, 100};
+        return (x / radix[d]) % 10;
     }
-    for(int i = 0; i < n; i++){
-        C[GetDigit(A[i], d)]++;
-    }
-    for(int i = 1; i < k; i++){
-        C[i] = C[i] + C[i - 1];
-    }
-    int *B = new int[n];
-    for(int i = n - 1; i >= 0; i--){
-        int dight = GetDigit(A[i], d);
-        B[--C[dight]] = A[i];
-    }
-    for(int i = 0; i < n; i++){
-        A[i] = B[i];
-    }
-    delete B;
-}
-
-void LsdRedixSort(int A[], int n){
-    for(int d = 1; d < dn; d++){
-        CountingSort(A, n, d);
-    }
-}
+};
 
 int main(){
     // int _source[] = {41, 67, 34, 0, 69, 24, 78, 58, 62, 64, 5, 45, 81, 27, 61, 91, 95, 42, 27, 36};
@@ -61,9 +64,13 @@ int main(){
     //     _source[i] = rand() % 100;
     // }
 
-    PA_DisplayData(_source, nCount);
-    LsdRedixSort(_source, nCount);
-    PA_DisplayData(_source, nCount);
+    LsdRedixSortDemo1 * lrsd = new LsdRedixSortDemo1();
+
+    lrsd->PA_DisplayData(_source, nCount);
+    lrsd->LsdRedixSort(_source, nCount);
+    lrsd->PA_DisplayData(_source, nCount);
+
+    delete lrsd;
 
     return 0;
 }
