6c0fabee355d3d66142052e1a6d13999219b834f
asm32cn <asm32@qq.com>
2018-05-22 00:06:32 +0800

    c-counting-sort-demo-1.cpp ... add

--- C-Arithmetic-2018/c-counting-sort-demo-1.cpp/c-counting-sort-demo-1.cpp
+++ C-Arithmetic-2018/c-counting-sort-demo-1.cpp/c-counting-sort-demo-1.cpp
@@ -3,36 +3,39 @@
 
 using namespace std;
 
-void PA_DisplayData(int data[], int nCount){
-    for(int i = 0; i < nCount; i++){
-        if(i) cout << ", ";
-        cout << data[i];
+class CountingSortDemo1{
+private:
+    const static int k = 100;
+    int C[k];
+public:
+    void PA_DisplayData(int data[], int nCount){
+        for(int i = 0; i < nCount; i++){
+            if(i) cout << ", ";
+            cout << data[i];
+        }
+        cout << endl;
     }
-    cout << endl;
-}
 
-const int k = 100;
-int C[k];
-
-void CountingSort(int A[], int n){
-    for(int i = 0; i < k; i++){
-        C[i] = 0;
+    void CountingSort(int A[], int n){
+        for(int i = 0; i < k; i++){
+            C[i] = 0;
+        }
+        for(int i = 0; i < n; i++){
+            C[A[i]]++;
+        }
+        for(int i = 1; i < k; i++){
+            C[i] = C[i] + C[i - 1];
+        }
+        int *B = new int[n];
+        for(int i = n - 1; i >= 0; i--){
+            B[--C[A[i]]] = A[i];
+        }
+        for(int i = 0; i < n; i++){
+            A[i] = B[i];
+        }
+        delete B;
     }
-    for(int i = 0; i < n; i++){
-        C[A[i]]++;
-    }
-    for(int i = 1; i < k; i++){
-        C[i] = C[i] + C[i - 1];
-    }
-    int *B = new int[n];
-    for(int i = n - 1; i >= 0; i--){
-        B[--C[A[i]]] = A[i];
-    }
-    for(int i = 0; i < n; i++){
-        A[i] = B[i];
-    }
-    delete B;
-}
+};
 
 int main(){
     // int _source[] = {41, 67, 34, 0, 69, 24, 78, 58, 62, 64, 5, 45, 81, 27, 61, 91, 95, 42, 27, 36};
@@ -46,9 +49,13 @@ int main(){
     //     _source[i] = rand() % 100;
     // }
 
-    PA_DisplayData(_source, nCount);
-    CountingSort(_source, nCount);
-    PA_DisplayData(_source, nCount);
-    
+    CountingSortDemo1 * csd = new CountingSortDemo1();
+
+    csd->PA_DisplayData(_source, nCount);
+    csd->CountingSort(_source, nCount);
+    csd->PA_DisplayData(_source, nCount);
+
+    delete csd;
+
     return 0;
 }
