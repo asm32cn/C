f7e361172acbd865f28d16eb37761f6e8bc47257
asm32cn <asm32@qq.com>
2018-05-25 02:23:04 +0800

    c-insertion-sort-demo-1.c ... add

--- C-Arithmetic-2018/c-insertion-sort-demo-1.c/c-insertion-sort-demo-1.c
+++ C-Arithmetic-2018/c-insertion-sort-demo-1.c/c-insertion-sort-demo-1.c
@@ -9,31 +9,31 @@ void PA_DisplayData(int data[], int nCount){
     printf("\n");
 }
 
-void InsertionSort(int A[], int n){
+void InsertionSort(int data[], int n){
     for(int i = 1; i < n; i++){
-        int get = A[i];
+        int get = data[i];
         int j = i - 1;
-        while(j >= 0 && A[j] > get){
-            A[j + 1] = A[j];
+        while(j >= 0 && data[j] > get){
+            data[j + 1] = data[j];
             j--;
         }
-        A[j + 1] = get;
+        data[j + 1] = get;
     }
 }
 
 void main(){
-    // int _source[] = {41, 67, 34, 0, 69, 24, 78, 58, 62, 64, 5, 45, 81, 27, 61, 91, 95, 42, 27, 36};
-    int _source[] = {76, 11, 11, 43, 78, 35, 39, 27, 16, 55, 1, 41, 24, 19, 54, 7, 78, 69, 65, 82};
-    int nCount = sizeof(_source) / sizeof(int);
+    // int data[] = {41, 67, 34, 0, 69, 24, 78, 58, 62, 64, 5, 45, 81, 27, 61, 91, 95, 42, 27, 36};
+    int data[] = {76, 11, 11, 43, 78, 35, 39, 27, 16, 55, 1, 41, 24, 19, 54, 7, 78, 69, 65, 82};
+    int nCount = sizeof(data) / sizeof(int);
 
     printf("Item count: %d\n", nCount);
 
     // srand(time(NULL));
     // for(int i = 0; i < 20; i++){
-    //     _source[i] = rand() % 100;
+    //     data[i] = rand() % 100;
     // }
 
-    PA_DisplayData(_source, nCount);
-    InsertionSort(_source, nCount);
-    PA_DisplayData(_source, nCount);
+    PA_DisplayData(data, nCount);
+    InsertionSort(data, nCount);
+    PA_DisplayData(data, nCount);
 }
