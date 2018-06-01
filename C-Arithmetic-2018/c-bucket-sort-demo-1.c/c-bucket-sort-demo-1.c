09ac4610b640debc425f974adc2b3bfea88e4864
asm32cn <asm32@qq.com>
2018-06-01 22:41:07 +0800

    c-bucket-sort-demo-1.c ... modify

--- C-Arithmetic-2018/c-bucket-sort-demo-1.c/c-bucket-sort-demo-1.c
+++ C-Arithmetic-2018/c-bucket-sort-demo-1.c/c-bucket-sort-demo-1.c
@@ -4,11 +4,11 @@
 // #include <time.h>
 
 void PA_DisplayData(int data[], int nCount){
-    for(int i = 0; i < nCount; i++){
-        if(i) printf(", ");
-        printf("%d", data[i]);
-    }
-    printf("\n");
+	for(int i = 0; i < nCount; i++){
+		if(i) printf(", ");
+		printf("%d", data[i]);
+	}
+	printf("\n");
 }
 
 #define MAX 100
@@ -19,66 +19,66 @@ int nFactor = MAX % bn ? MAX / bn + 1 : MAX / bn;
 int C[bn];
 
 void InsertionSort(int data[], int nLeft, int nRight){
-    for(int i = nLeft + 1; i <= nRight; i++){
-        int nGet = data[i];
-        int j = i - 1;
-        while(j >= nLeft && data[j] > nGet){
-            data[j + 1] = data[j];
-            j--;
-        }
-        data[j + 1] = nGet;
-    }
+	for(int i = nLeft + 1; i <= nRight; i++){
+		int nGet = data[i];
+		int j = i - 1;
+		while(j >= nLeft && data[j] > nGet){
+			data[j + 1] = data[j];
+			j--;
+		}
+		data[j + 1] = nGet;
+	}
 }
 
 int MapToBucket(int x){
-    return x / nFactor;
+	return x / nFactor;
 }
 
 void CountingSort(int data[], int n){
-    for(int i = 0; i < bn; i++){
-        C[i] = 0;
-    }
-    for(int i = 0; i < n; i++){
-        C[MapToBucket(data[i])]++;
-    }
-    for(int i = 1; i < bn; i++){
-        C[i] = C[i] + C[i - 1];
-    }
-    int * B = (int *)malloc(n * sizeof(int));
-    for(int i = n - 1; i >= 0; i--){
-        int b = MapToBucket(data[i]);
-        B[--C[b]] = data[i];
-    }
-    for(int i = 0; i < n; i++){
-        data[i] = B[i];
-    }
-    free(B);
+	for(int i = 0; i < bn; i++){
+		C[i] = 0;
+	}
+	for(int i = 0; i < n; i++){
+		C[MapToBucket(data[i])]++;
+	}
+	for(int i = 1; i < bn; i++){
+		C[i] = C[i] + C[i - 1];
+	}
+	int * B = (int *)malloc(n * sizeof(int));
+	for(int i = n - 1; i >= 0; i--){
+		int b = MapToBucket(data[i]);
+		B[--C[b]] = data[i];
+	}
+	for(int i = 0; i < n; i++){
+		data[i] = B[i];
+	}
+	free(B);
 }
 
 void BucketSort(int data[], int n){
-    CountingSort(data, n);
-    for(int i = 0; i < bn; i++){
-        int nLeft = C[i];
-        int nRight = (i == bn - 1 ? n - 1 : C[i + 1] - 1); // C[i + 1] - 1为i号桶最后一个元素的位置
-        if(nLeft < nRight){
-            InsertionSort(data, nLeft, nRight);
-        }
-    }
+	CountingSort(data, n);
+	for(int i = 0; i < bn; i++){
+		int nLeft = C[i];
+		int nRight = (i == bn - 1 ? n - 1 : C[i + 1] - 1); // C[i + 1] - 1为i号桶最后一个元素的位置
+		if(nLeft < nRight){
+			InsertionSort(data, nLeft, nRight);
+		}
+	}
 }
 
 void main(){
-    // int data[] = {41, 67, 34, 0, 69, 24, 78, 58, 62, 64, 5, 45, 81, 27, 61, 91, 95, 42, 27, 36};
-    int data[] = {76, 11, 11, 43, 78, 35, 39, 27, 16, 55, 1, 41, 24, 19, 54, 7, 78, 69, 65, 82};
-    int nCount = sizeof(data) / sizeof(int);
+	// int data[] = {41, 67, 34, 0, 69, 24, 78, 58, 62, 64, 5, 45, 81, 27, 61, 91, 95, 42, 27, 36};
+	int data[] = {76, 11, 11, 43, 78, 35, 39, 27, 16, 55, 1, 41, 24, 19, 54, 7, 78, 69, 65, 82};
+	int nCount = sizeof(data) / sizeof(int);
 
-    printf("Item count: %d\n", nCount);
+	printf("Item count: %d\n", nCount);
 
-    // srand(time(NULL));
-    // for(int i = 0; i < 20; i++){
-    //     data[i] = rand() % 1000;
-    // }
+	// srand(time(NULL));
+	// for(int i = 0; i < 20; i++){
+	// 	data[i] = rand() % 1000;
+	// }
 
-    PA_DisplayData(data, nCount);
-    BucketSort(data, nCount);
-    PA_DisplayData(data, nCount);
+	PA_DisplayData(data, nCount);
+	BucketSort(data, nCount);
+	PA_DisplayData(data, nCount);
 }
