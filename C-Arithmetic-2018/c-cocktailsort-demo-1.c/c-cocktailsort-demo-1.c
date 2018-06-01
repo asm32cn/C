382dc4b4f81ca2a14a4635d8bff09a6e60905b47
asm32cn <asm32@qq.com>
2018-06-01 22:42:56 +0800

    c-cocktailsort-demo-1.c ... modify

--- C-Arithmetic-2018/c-cocktailsort-demo-1.c/c-cocktailsort-demo-1.c
+++ C-Arithmetic-2018/c-cocktailsort-demo-1.c/c-cocktailsort-demo-1.c
@@ -3,53 +3,53 @@
 #include <stdio.h>
 
 void PA_DisplayData(int data[], int nCount){
-    for(int i = 0; i < nCount; i++){
-        if(i) printf(", ");
-        printf("%d", data[i]);
-    }
-    printf("\n");
+	for(int i = 0; i < nCount; i++){
+		if(i) printf(", ");
+		printf("%d", data[i]);
+	}
+	printf("\n");
 }
 
 void Swap(int data[], int i, int j){
-    int temp = data[i];
-    data[i] = data[j];
-    data[j] = temp;
+	int temp = data[i];
+	data[i] = data[j];
+	data[j] = temp;
 }
 
 void CocktailSort(int data[], int n){
-    int left = 0;
-    int right = n - 1;
-    while(left < right){
-        for(int i = left; i < right; i++){
-            if(data[i] > data[i + 1]){
-                Swap(data, i, i + 1);
-            }
-        }
-        right--;
-        for(int i = right; i > left; i--){
-            if(data[i - 1] > data[i]){
-                Swap(data, i - 1, i);
-            }
-        }
-        left++;
-    }
+	int left = 0;
+	int right = n - 1;
+	while(left < right){
+		for(int i = left; i < right; i++){
+			if(data[i] > data[i + 1]){
+				Swap(data, i, i + 1);
+			}
+		}
+		right--;
+		for(int i = right; i > left; i--){
+			if(data[i - 1] > data[i]){
+				Swap(data, i - 1, i);
+			}
+		}
+		left++;
+	}
 }
 
 int main(){
    // int data[] = {41, 67, 34, 0, 69, 24, 78, 58, 62, 64, 5, 45, 81, 27, 61, 91, 95, 42, 27, 36};
-    int data[] = {76, 11, 11, 43, 78, 35, 39, 27, 16, 55, 1, 41, 24, 19, 54, 7, 78, 69, 65, 82};
-    int nCount = sizeof(data) / sizeof(int);
+	int data[] = {76, 11, 11, 43, 78, 35, 39, 27, 16, 55, 1, 41, 24, 19, 54, 7, 78, 69, 65, 82};
+	int nCount = sizeof(data) / sizeof(int);
 
-    printf("Item count: %d\n", nCount);
+	printf("Item count: %d\n", nCount);
 
-    // srand(time(NULL));
-    // for(int i = 0; i < 20; i++){
-    //     data[i] = rand() % 100;
-    // }
+	// srand(time(NULL));
+	// for(int i = 0; i < 20; i++){
+	//     data[i] = rand() % 100;
+	// }
 
-    PA_DisplayData(data, nCount);
-    CocktailSort(data, nCount);
-    PA_DisplayData(data, nCount);
+	PA_DisplayData(data, nCount);
+	CocktailSort(data, nCount);
+	PA_DisplayData(data, nCount);
 
-    return 0;
+	return 0;
 }
