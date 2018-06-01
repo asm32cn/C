5f6143914fca3467dae7d64c3db10a4b7f12a069
asm32cn <asm32@qq.com>
2018-06-01 22:34:38 +0800

    c-selectionsort-demo-1.c ... modify

--- C-Arithmetic-2018/c-selectionsort-demo-1.c/c-selectionsort-demo-1.c
+++ C-Arithmetic-2018/c-selectionsort-demo-1.c/c-selectionsort-demo-1.c
@@ -4,47 +4,47 @@
 
 
 void PA_DisplayData(int data[], int nCount){
-    for(int i = 0; i < nCount; i++){
-        if(i) printf(", ");
-        printf("%d", data[i]);
-    }
-    printf("\n");
+	for(int i = 0; i < nCount; i++){
+		if(i) printf(", ");
+		printf("%d", data[i]);
+	}
+	printf("\n");
 }
 
 void Swap(int data[], int i, int j){
-    int temp = data[i];
-    data[i] = data[j];
-    data[j] = temp;
+	int temp = data[i];
+	data[i] = data[j];
+	data[j] = temp;
 }
 
 void SelectionSort(int data[], int n){
-    for(int i = 0; i < n - 1; i++){
-        int min = i;
-        for(int j = i + 1; j < n; j++){
-            if(data[j] < data[min]){
-                min = j;
-            }
-        }
-        if(min != i){
-            Swap(data, min, i);
-        }
-    }
+	for(int i = 0; i < n - 1; i++){
+		int nMin = i;
+		for(int j = i + 1; j < n; j++){
+			if(data[j] < data[nMin]){
+				nMin = j;
+			}
+		}
+		if(nMin != i){
+			Swap(data, nMin, i);
+		}
+	}
 }
 
 
 void main(){
-    // int data[] = {41, 67, 34, 0, 69, 24, 78, 58, 62, 64, 5, 45, 81, 27, 61, 91, 95, 42, 27, 36};
-    int data[] = {76, 11, 11, 43, 78, 35, 39, 27, 16, 55, 1, 41, 24, 19, 54, 7, 78, 69, 65, 82};
-    int nCount = sizeof(data) / sizeof(int);
+	// int data[] = {41, 67, 34, 0, 69, 24, 78, 58, 62, 64, 5, 45, 81, 27, 61, 91, 95, 42, 27, 36};
+	int data[] = {76, 11, 11, 43, 78, 35, 39, 27, 16, 55, 1, 41, 24, 19, 54, 7, 78, 69, 65, 82};
+	int nCount = sizeof(data) / sizeof(int);
 
-    printf("Item count: %d\n", nCount);
+	printf("Item count: %d\n", nCount);
 
-    // srand(time(NULL));
-    // for(int i = 0; i < 20; i++){
-    //     data[i] = rand() % 100;
-    // }
+	// srand(time(NULL));
+	// for(int i = 0; i < 20; i++){
+	//     data[i] = rand() % 100;
+	// }
 
-    PA_DisplayData(data, nCount);
-    SelectionSort(data, nCount);
-    PA_DisplayData(data, nCount);
+	PA_DisplayData(data, nCount);
+	SelectionSort(data, nCount);
+	PA_DisplayData(data, nCount);
 }
