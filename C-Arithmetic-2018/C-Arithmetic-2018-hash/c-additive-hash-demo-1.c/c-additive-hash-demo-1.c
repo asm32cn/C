73d49bb29e4cf0e7c9b3005d08e7e76cb9a65cc3
asm32cn <asm32@qq.com>
2018-06-09 14:56:51 +0800

    c-additive-hash-demo-1.c ... modify

--- C-Arithmetic-2018/C-Arithmetic-2018-hash/c-additive-hash-demo-1.c/c-additive-hash-demo-1.c
+++ C-Arithmetic-2018/C-Arithmetic-2018-hash/c-additive-hash-demo-1.c/c-additive-hash-demo-1.c
@@ -15,21 +15,21 @@ void main(){
 	int nCount = sizeof(A_strKeys) / sizeof(char *);
 
 	for(int i = 0; i < nCount; i++){
-		printf("%2d %12s %10d\n", i, A_strKeys[i], additiveHash(A_strKeys[i],31));
+		printf("%-10d %-15s %3d\n", i, A_strKeys[i], additiveHash(A_strKeys[i],31));
 	}
 }
 
 /*
- 0            C          6
- 1          C++          1
- 2         Java         18
- 3           C#         11
- 4       Python         28
- 5           Go         29
- 6        Scala         24
- 7       vb.net          6
- 8   JavaScript          2
- 9          PHP         18
-10         Perl          4
-11         Ruby         19
+0          C                 6
+1          C++               1
+2          Java             18
+3          C#               11
+4          Python           28
+5          Go               29
+6          Scala            24
+7          vb.net            6
+8          JavaScript        2
+9          PHP              18
+10         Perl              4
+11         Ruby             19
 */
