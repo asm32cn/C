78cbcee83f57589767b3aab9cade9e7bfc4c55c4
asm32cn <asm32@qq.com>
2018-07-01 23:52:39 +0800

    c-base64-demo-1.c ... modify

--- C-Arithmetic-2018/C-Arithmetic-2018-encrypt/c-base64-demo-1.c/c-base64-demo-1.c
+++ C-Arithmetic-2018/C-Arithmetic-2018-encrypt/c-base64-demo-1.c/c-base64-demo-1.c
@@ -8,43 +8,34 @@ void DisplayHexString(unsigned char * pszData){
 	unsigned char ch, * psz = pszData;
 	unsigned char szLine[100] = {0};
 	int n = 0, nCol = 0, nOffset = 0;
-	szLine[0] = 0;
+
 	for(int i = 0; i < 64; i++) szLine[i] = 32;
 	szLine[23] = '-';
-	szLine[48] = '.';
-	szLine[66] = '#';
-	szLine[67] = 10;
+	szLine[64] = 10;
 	while(ch = *psz++){
 		nCol = n % 16;
-		if(nCol == 0){
-			printf("\n");
+		if(n && nCol == 0){
 			printf(szLine);
 			for(int i = 0; i < 64; i++) szLine[i] = 32;
 			szLine[23] = '-';
-			szLine[48] = '.';
-			szLine[66] = '#';
-			szLine[67] = 10;
-			printf("\n");
 			nOffset = 0;
 		}
 
-		printf("%02X ", ch);
 		szLine[nOffset] = szHexDigit[ch >> 4];
 		szLine[nOffset + 1] = szHexDigit[ch % 16];
-		szLine[nCol + 48] = ch < 32 ? '.' : ch;
+		szLine[nCol + 48] = ch < 32 || ch > 127 ? '.' : ch;
 		nOffset += 3;
 		n++;
 	}
 	if(nCol){
-		printf("\n");
 		printf(szLine);
 	}
-	printf("\n");
 }
 
 void main(){
 	unsigned char * pszData = "go-base64-demo-1.go\n程序中书写着所见所闻所感，编译着心中的万水千山。";
 	printf("c-base64-demo-1.c\n");
 	DisplayHexString(pszData);
+	printf("%s\n", pszData);
 }
 
