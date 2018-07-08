1d500c5f3d48334d823fe5e000129db679b59a0c
asm32cn <asm32@qq.com>
2018-07-09 00:17:07 +0800

    MessageBoxDemo1.cpp ... modify

--- C-Training-2018-win32/MessageBoxDemo1.cpp/MessageBoxDemo1.cpp
+++ C-Training-2018-win32/MessageBoxDemo1.cpp/MessageBoxDemo1.cpp
@@ -1,7 +1,11 @@
 // MessageBoxDemo1.cpp
-#include "windows.h"
+#include <windows.h>     
 
-int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, PSTR szCmdLine, int nCmdShow){
-	MessageBox(NULL, TEXT("你好，欢迎来到VC世界"), TEXT("欢迎"), MB_ICONINFORMATION);
-	return 0;
+int APIENTRY WinMain(HINSTANCE hInstance, //当前实例句柄     
+		HINSTANCE hPrevInstance,          //前一个实例句柄，Win32下为NULL     
+		LPSTR     lpCmdLine,              //命令行参数字符     
+		int       nCmdShow)               //窗口的显示方式     
+{
+    MessageBox(NULL,"欧阳软件学习网欢迎您！","欢迎你",MB_OK | MB_ICONINFORMATION); //通用对话框     
+    return 0;  //函数返回值     
 }
