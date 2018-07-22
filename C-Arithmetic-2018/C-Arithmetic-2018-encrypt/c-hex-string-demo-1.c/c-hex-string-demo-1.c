a39ed930b64f93af4da7410fb6ce6a5ca5598b15
asm32cn <asm32@qq.com>
2018-07-23 00:29:16 +0800

    c-hex-string-demo-1.c ... modify

--- C-Arithmetic-2018/C-Arithmetic-2018-encrypt/c-hex-string-demo-1.c/c-hex-string-demo-1.c
+++ C-Arithmetic-2018/C-Arithmetic-2018-encrypt/c-hex-string-demo-1.c/c-hex-string-demo-1.c
@@ -19,10 +19,16 @@ void HexStringDecode(unsigned char * lpszSource, unsigned char * lpszTarget){
 	unsigned char * pszSource = lpszSource, * pszTarget = lpszTarget;
 	unsigned char ch1, ch2;
 	while(*pszSource){
-		ch1 = *pszSource++;
-		ch2 = *pszSource++;
-		// ch1 
+		ch1 = (*pszSource++) % 48;
+		ch2 = (*pszSource++) % 48;
+		if(ch1 > 16) ch1 -= 7;
+		if(ch2 > 16) ch2 -= 7;
+		ch1 = ch1 & 15;
+		ch2 = ch2 & 15;
+
+		*pszTarget++ = (ch1 << 4) + ch2;
 	}
+	*pszTarget = 0;
 }
 
 void main(){
@@ -31,7 +37,17 @@ void main(){
 	unsigned char * pszHexStringEncode = malloc(nCount * 2 + 1);
 	unsigned char * pszHexStringDecode = malloc(nCount + 1);
 
-	HexStringEncode(szData, pszHexString);
-	printf("%s\n%s\n", szData, pszHexString);
-	free(pszHexString);
+	HexStringEncode(szData, pszHexStringEncode);
+	HexStringDecode(pszHexStringEncode, pszHexStringDecode);
+	printf("%s\n%s\n%s\n", szData, pszHexStringEncode, pszHexStringDecode);
+	free(pszHexStringEncode);
+	free(pszHexStringDecode);
 }
+
+/*
+c-base64-demo-1.c
+程序中书写着所见所闻所感，编译着心中的万水千山。
+632D6261736536342D64656D6F2D312E630AE7A88BE5BA8FE4B8ADE4B9A6E58699E79D80E68980E8A781E68980E997BBE68980E6849FEFBC8CE7BC96E8AF91E79D80E5BF83E4B8ADE79A84E4B887E6B0B4E58D83E5B1B1E38082
+c-base64-demo-1.c
+程序中书写着所见所闻所感，编译着心中的万水千山。
+*/
